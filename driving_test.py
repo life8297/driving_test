a7bc6d92f45028a564dae873a2b0adb74d939a8a
JR <allen8297@gmail.com>
2020-07-08 23:01:47 +0800

    Add if Taiwan

--- driving_test.py
+++ driving_test.py
@@ -1,4 +1,4 @@
-country = input('請輸入來自哪個國家:')
+country = input('請輸入來自哪個國家(台灣/美國):')
 age = input('請輸入年齡:')
 age = int(age)
 if country == '台灣':
@@ -12,4 +12,4 @@ elif country =="美國":
 	else:
 		print('你還不能考駕照')
 else:
-	print('請洽尋求專員考取駕照問題')
+	print('請洽尋專員考取駕照問題')
